47539e90d3ad7cfbb121e9bab8c9bc47e92cb8fc
matan2288 <matan2288@gmail.com>
2026-02-23 14:19:43 +0200

    Added pipes x reset const

--- components/game/Pipes/Pipes.tsx
+++ components/game/Pipes/Pipes.tsx
@@ -1,7 +1,7 @@
 import { Component, createRef } from 'react';
 import { Image, Dimensions } from "react-native";
 import { PipesStateInterface } from './Pipes.types';
-import { PIPE_HEIGHT_RATIO, RANDOM_OFFSET_MAX, RANDOM_OFFSET_MIN, PIPE_WIDTH, TOP_PIPE_DRAW_START, BOTTOM_PIPE_DRAW_START } from './pipesConsts';
+import { PIPE_HEIGHT_RATIO, RANDOM_OFFSET_MAX, RANDOM_OFFSET_MIN, PIPE_WIDTH, TOP_PIPE_DRAW_START, BOTTOM_PIPE_DRAW_START, PIPE_RESET_X_POSITION } from './pipesConsts';
 
 
 class Pipes extends Component<{}, PipesStateInterface> {
@@ -38,9 +38,8 @@ class Pipes extends Component<{}, PipesStateInterface> {
     }
 
     resetPipes() {
-        const { width } = Dimensions.get('window');
         this.setState({
-            pipesXposition: 0
+            pipesXposition: PIPE_RESET_X_POSITION
         });
     }
 
--- components/game/Pipes/pipesConsts.ts
+++ components/game/Pipes/pipesConsts.ts
@@ -6,3 +6,4 @@ export const RANDOM_OFFSET_MIN = 0.1;
 export const PIPE_WIDTH = 80;
 export const TOP_PIPE_DRAW_START = 0;
 export const BOTTOM_PIPE_DRAW_START = 0;
+export const PIPE_RESET_X_POSITION = 0;
