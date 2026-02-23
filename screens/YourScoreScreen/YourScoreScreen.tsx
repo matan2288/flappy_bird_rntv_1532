e7ae7e3f14ce0964978e578a0b33ba207df4949d
matan2288 <matan2288@gmail.com>
2026-02-23 16:22:34 +0200

    Added user info to your score screen

--- screens/YourScoreScreen/YourScoreScreen.tsx
+++ screens/YourScoreScreen/YourScoreScreen.tsx
@@ -2,13 +2,19 @@ import { Text, View, Pressable } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { RootStackParamList } from "@/navigation/routes";
 import { globalStyles } from "@/theme/globalStyles";
+import { useUserStore } from '@/store';
+
 
 type Props = NativeStackScreenProps<RootStackParamList, "YourScore">;
 
 export default function YourScoreScreen({ navigation }: Props) {
+    const { username, score } = useUserStore(state => state)
+
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.title}>Your Score</Text>
+
+            <Text style={globalStyles.buttonText}>{username}, Your Score is:</Text>
+            <Text style={globalStyles.title}>{score}</Text>
 
             <View style={globalStyles.buttonContainer}>
                 <Pressable
