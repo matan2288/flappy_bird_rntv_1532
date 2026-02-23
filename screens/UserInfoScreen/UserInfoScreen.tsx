0b422a5b487314dfa4631f50bdbbb5601445b448
matan2288 <matan2288@gmail.com>
2026-02-23 16:11:35 +0200

    Register username to global state

--- screens/UserInfoScreen/UserInfoScreen.tsx
+++ screens/UserInfoScreen/UserInfoScreen.tsx
@@ -1,16 +1,36 @@
-import { Text, View, Pressable } from "react-native";
+import { Text, TextInput, View, Pressable } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import type { RootStackParamList } from "@/navigation/routes";
 import { globalStyles } from "@/theme/globalStyles";
+import { useUserStore } from '@/store';
 
 type Props = NativeStackScreenProps<RootStackParamList, "UserInfo">;
 
 export default function UserInfoScreen({ navigation }: Props) {
+    const { username, registerUsername } = useUserStore(state => state)
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.title}>User Info</Text>
 
             <View style={globalStyles.buttonContainer}>
+                <Text style={{ marginVertical: 8, color: 'white' }}>Enter your name:</Text>
+                <View style={{
+                    backgroundColor: '#fff',
+                    borderRadius: 8,
+                    paddingHorizontal: 10,
+                    marginBottom: 10,
+                    borderWidth: 1,
+                    borderColor: '#ccc'
+                }}>
+                    <TextInput
+                        placeholder="Your name"
+                        style={{ height: 40 }}
+                        value={username}
+                        onChangeText={registerUsername}
+                        autoCapitalize="words"
+                    />
+                </View>
+
                 <Pressable
                     style={({ pressed }) => [
                         globalStyles.button,
@@ -18,8 +38,11 @@ export default function UserInfoScreen({ navigation }: Props) {
                     ]}
                     onPress={() => navigation.navigate("GameScreen")}
                 >
+
                     <Text style={globalStyles.buttonText}>Start</Text>
+
                 </Pressable>
+
 
                 <Pressable
                     style={({ pressed }) => [
